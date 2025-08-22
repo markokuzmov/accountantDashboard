235f6d46f6670271de70b20dfad3f29ff71ecdd2
markokuzmov <marko.kuzmov@gmail.com>
2025-08-22 17:18:38 -0400

    improve last_reconciled date fetching, implemented it into Flask app and display account info in index

--- last_reconciled.py
+++ last_reconciled.py
@@ -1,23 +1,41 @@
 from playwright.sync_api import sync_playwright
 
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=False, slow_mo=500)
-    page = browser.new_page()
-    
-    page.goto("https://sandbox.qbo.intuit.com/")
-    print("Going to quickbooks")
-    
-    page.fill("#iux-identifier-first-international-email-user-id-input", "marko.kuzmov@gmail.com")
-    print("Typed in email")
-    page.click("text=Sign in")
-    print("Clicked sign in")
-    
-    page.wait_for_url("https://sandbox.qbo.intuit.com/app/homepage")
-    
-    page.goto("https://sandbox.qbo.intuit.com/app/reconcile")
-    print("Went to reconcile")
-    
-    text = page.locator('[data-testid="navigate-summary-report"]').inner_text()
-    print(text)
-    
-    browser.close()
+def get_last_reconciled_dates(account_id_list):
+
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+
+        page.goto("https://sandbox.qbo.intuit.com/")
+        print("Going to quickbooks")
+
+        page.fill("#iux-identifier-first-international-email-user-id-input", "marko.kuzmov@gmail.com")
+        print("Typed in email")
+        page.click("text=Sign in")
+        print("Clicked sign in")
+
+        page.wait_for_url("https://sandbox.qbo.intuit.com/app/homepage")
+        print("Loaded into homepage")
+
+        date_list = dict()
+        for id in account_id_list:
+            page.goto(f"https://sandbox.qbo.intuit.com/app/reconcile?accountId={id}")
+            
+            a_tag = page.locator('[data-testid="navigate-summary-report"]')
+            try:
+                a_tag.wait_for(timeout=1500)
+                print(f"found <a> tag in account id {id}")
+                
+                date = a_tag.inner_text()
+                date = date.replace("Last statement ending date ", "").strip()
+            except:
+                date = "N/A"
+                print(f"couldn't find date for account id {id}")
+
+            date_list[f"{id}"] = date
+            
+        browser.close()
+        
+        return date_list
+        
+
--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from flask import Flask, render_template, redirect, url_for, request
 
 import requests
 
+from last_reconciled import get_last_reconciled_dates
+
 app = Flask(__name__)
 
 load_dotenv()
@@ -15,6 +17,7 @@ CLIENT_ID = os.getenv('CLIENT_ID')
 CLIENT_SECRET = os.getenv('CLIENT_SECRET')
 
 REDIRECT_URI = 'http://localhost:5000/auth/code'
+BASE_URL = 'https://sandbox-quickbooks.api.intuit.com'
 
 
 auth_client = AuthClient(
@@ -34,7 +37,14 @@ auth_url = auth_client.get_authorization_url(scopes)
 @app.route("/", methods=['GET'])
 def index():
     logged_in = os.getenv('ACCESS_TOKEN') is not None
-    return render_template("index.html", logged_in=logged_in)
+    accounts = []
+    last_reconciled_dates = []
+    
+    if logged_in:
+        accounts = get_accounts_list()
+        last_reconciled_dates = get_last_reconciled_dates(get_account_id_list(accounts))
+    
+    return render_template("index.html", logged_in=logged_in, accounts=accounts, last_reconciled_dates=last_reconciled_dates)
 
 
 @app.route("/auth", methods=['GET'])
@@ -46,6 +56,7 @@ def auth():
 def get_token():
     code = request.args.get('code')
     realm_id = request.args.get('realmId')
+    os.environ["REALM_ID"] = realm_id
     
     auth_client.get_bearer_token(auth_code=code, realm_id=realm_id)
     
@@ -55,13 +66,35 @@ def get_token():
     return redirect(url_for('index'))
 
 
-def get_company_data():
+def get_accounts_list():
     access_token = os.getenv('ACCESS_TOKEN')
-    url = 'https://sandbox.api.intuit.com'
+    realm_id = os.getenv('REALM_ID')
+    
+    url = BASE_URL + f'/v3/company/{realm_id}/query'
+    query = "select Id, Name, AccountType, AccountSubType, Active from Account"
     
     response = requests.get(
-        
+        url,
+        headers= {
+            "Authorization": f"Bearer {access_token}",
+            "Accept": "application/json"
+        },
+        params={
+            "query": query
+        }
     )
+
+    data = response.json()
+    accounts = data.get("QueryResponse", {}).get("Account", [])
+    
+    return accounts
+
+def get_account_id_list(accounts):
+    account_id_list = []
+    for account in accounts:
+        account_id_list.append(account["Id"])
+    
+    return account_id_list
 
 
 if __name__ == "__main__":
